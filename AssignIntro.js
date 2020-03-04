2f4451cb1c77d2577fc1fe03dd61183d5676094a
rowena-johnson <200425021@student.georgianc.on.ca>
2020-03-03 22:47:41 -0500

    first commit

---
+++ AssignIntro.js
@@ -0,0 +1,59 @@
+const menu = [{
+title: 'Soup',
+soup: 'Minestrone',
+soupPrice: '3.25',
+title: 'Salad',
+Salad:'Greek Salad',
+saladPrice:'3.45',
+title: 'lighter fare',
+lighterFare: 'Vegetable Biriyani',
+lighterFarePrice:'5.95',
+title:'Entree',
+Entree: 'Breaded Chicken on a Bun',
+Entree: '5.95'
+},{
+    title: 'Soup',
+    soup: 'Red Lentil Dal',
+    soupPrice: '4.25',
+    title: 'Salad',
+    Salad:'Julienne Salad',
+    saladPrice:'3.45',
+    title: 'lighter fare',
+    lighterFare: 'Madras Chicken Salad Wrap',
+    lighterFarePrice:'5.95',
+    title:'Entree',
+    Entree: 'Fish & chips',
+    Entree: '6.95'
+},
+{
+    title: 'Soup',
+    soup: 'spicy squash & pumpkin',
+    soupPrice: '4.25',
+    title: 'Salad',
+    Salad:'Mediterranean salad',
+    saladPrice:'3.45',
+    title: 'lighter fare',
+    lighterFare: 'Ham & cheese panini',
+    lighterFarePrice:'5.95',
+    title:'Entree',
+    Entree: 'Butter Chicken with rice Pilaf',
+    Entree: '9.00'
+},
+{
+    {
+        title: 'Soup',
+        soup: 'Clam Chowder',
+        soupPrice: '9.25',
+        title: 'Salad',
+        Salad:'Isreli Couscous & feta',
+        saladPrice:'3.45',
+        title: 'lighter fare',
+        lighterFare: 'Pulled pork on a Bun',
+        lighterFarePrice:'10.95',
+        title:'Entree',
+        Entree: 'Matar Paneer & Basmati Rice',
+        Entree: '8.00'
+    }
+}
+]
+ 
